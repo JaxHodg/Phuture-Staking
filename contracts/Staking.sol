fcee08fb66a1e1083e777e1339976de935600722
Jax Hodgkinson <jax.h@outlook.com>
2022-04-08 22:01:30 -0400

    Several changes - Changed variable names & function headers to be closer to 4626 standard - Added comments to every function - Added new functions maxReward & gcf - Fixed reward calculation (Solidity has no floating point numbers)

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -5,56 +5,147 @@ import "./IERC20.sol";
 import "./MyToken.sol" as MyToken;
 
 contract Staking {
-    IERC20 public Token;
+    IERC20 public asset;
 
-    uint256 public Total;
-    uint256 public S;
-    mapping(address => uint256) public Staked;
-    mapping(address => uint256) public S_0;
+    uint256 public totalAssets;
+    uint256 public s_num;
+    uint256 public s_den;
+    mapping(address => uint256) public staked;
+    mapping(address => uint256) public s_0_num;
+    mapping(address => uint256) public s_0_den;
 
-    constructor(address _token) {
-        Token = IERC20(_token);
-        Total = 0;
-        S = 0;
+    constructor(address _asset) {
+        asset = IERC20(_asset);
+        totalAssets = 0;
+        s_num = 0;
+        s_den = 1;
     }
 
-    function Stake(address from, uint256 amount) public {
-        require(Token.transferFrom(from, address(this), amount));
+    /**
+     * @notice Function to stake a specific amount of tokens
+     * @param assets number of tokens to stake
+     * @param receiver address to stake tokens for
+     * @return success boolean of whether stake was unsuccessful
+     */
+    function stake(uint256 assets, address receiver) public returns (bool) {
+        require(assets != 0);
+        require(asset.transferFrom(receiver, address(this), assets));
 
-        Staked[from] = amount;
-        S_0[from] = S;
-        Total = Total + amount;
+        uint256 maxAssets = maxUnstake(receiver);
+        uint256 rewards = maxRewards(receiver);
+        if (maxAssets != 0) {
+            totalAssets += assets + rewards;
+            staked[receiver] += assets + rewards;
+        } else {
+            totalAssets += assets;
+            staked[receiver] = assets;
+        }
+        s_0_num[receiver] = s_num;
+        s_0_den[receiver] = s_den;
+
+        return true;
     }
 
-    function Unstake(
-        address from,
-        address to,
-        uint256 amount
-    ) public {
-        require(msg.sender == from);
+    /**
+     * @notice Function to unstake a specfic amount of tokens staked + rewards
+     * @param assets number of tokens to be withdrawn
+     * @param receiver address to transfer tokens to
+     * @param owner address to withdraw stake and rewards from
+     * @return success boolean of whether unstake was unsuccessful
+     */
+    function unstake(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public returns (bool) {
+        require(msg.sender == owner);
 
-        uint256 staked = Staked[from];
+        uint256 maxAssets = maxUnstake(owner);
+        require(maxAssets >= assets);
 
-        uint256 maxUnstake = MaxUnstake(from);
-        require(maxUnstake >= amount);
+        totalAssets = totalAssets - staked[owner] + maxAssets - assets;
+        staked[owner] = maxAssets - assets;
 
-        Staked[from] = maxUnstake - amount;
-        Total = Total - staked;
-        S_0[from] = S;
+        s_0_num[owner] = s_num;
+        s_0_den[owner] = s_den;
 
-        require(Token.transfer(to, amount));
+        require(asset.transfer(receiver, assets));
+        return true;
     }
 
-    function MaxUnstake(address from) public view returns (uint256) {
-        uint256 staked = Staked[from];
-        uint256 reward = staked * (S - S_0[from]);
-        return staked + reward;
+    /**
+     * @notice Function to calculate the maximum tokens withdrawable
+     * @param owner address to calculate rewards for
+     * @return maxAssets number of tokens that be withdrawn for the address
+     */
+    function maxUnstake(address owner) public view returns (uint256) {
+        if (staked[owner] == 0) {
+            return 0;
+        }
+        uint256 reward = maxRewards(owner);
+        return staked[owner] + reward;
     }
 
-    function Distribute(address from, uint256 amount) public {
-        require(Token.transferFrom(from, address(this), amount));
+    /**
+     * @notice Function to calculate current rewards owed on stake
+     * @param owner address to calculate rewards for
+     * @return maxRewards number of tokens assigned as rewards
+     */
+    function maxRewards(address owner) public view returns (uint256) {
+        if (staked[owner] == 0) {
+            return 0;
+        }
+        uint256 reward_a = staked[owner] * (s_num);
+        reward_a /= s_den;
+        uint256 reward_b = staked[owner] * s_0_num[owner];
+        reward_b /= s_0_den[owner];
 
-        require(Total != 0);
-        S += amount / Total;
+        return reward_a - reward_b;
+    }
+
+    function whatMaxRewards(address owner) public view returns (uint256) {
+        if (staked[owner] == 0) {
+            return 0;
+        }
+        uint256 reward = ((staked[owner] * s_num) / s_den) -
+            ((staked[owner] * s_0_num[owner]) / s_0_den[owner]);
+
+        return reward;
+    }
+
+    /**
+     * @notice Function to distribute rewards to stakers
+     * @param assets amount of reward tokens to transfer
+     * @param sender address to transfer reward tokens from (must have approval)
+     */
+    function distribute(uint256 assets, address sender) public returns (bool) {
+        require(asset.transferFrom(sender, address(this), assets));
+
+        require(totalAssets != 0);
+
+        s_num = s_num * totalAssets + s_den * assets;
+        s_den = s_den * totalAssets;
+
+        uint256 divisor = gcf(s_num, s_den);
+        s_num /= divisor;
+        s_den /= divisor;
+
+        return true;
+    }
+
+    /**
+     * @notice Function to calculate the greatest common factor of two numbers
+     * @param a the first number
+     * @param b the second number
+     * @return gcf the greatest common factor of the two numbers
+     */
+    function gcf(uint256 a, uint256 b) private pure returns (uint256) {
+        uint256 r = a % b;
+        while (r != 0) {
+            a = b;
+            b = r;
+            r = a % b;
+        }
+        return b;
     }
 }
--- test/Stake.js
+++ test/Stake.js
@@ -2,13 +2,14 @@ const { expect } = require("chai");
 
 describe("Staking contracts", function () {
   let addr1;
+  let addr2;
 
   let MyToken;
 
   let Staking;
 
   beforeEach(async function () {
-    [addr1] = await ethers.getSigners();
+    [addr1, addr2] = await ethers.getSigners();
 
     MyToken = await ethers.getContractFactory("MyToken");
     Staking = await ethers.getContractFactory("Staking");
@@ -17,51 +18,197 @@ describe("Staking contracts", function () {
     Staking = await Staking.deploy(MyToken.address);
   });
 
-  it("Addr1 should have full supply", async function () {
-    const addr1Balance = await MyToken.balanceOf(addr1.address);
-    expect(await MyToken.totalSupply()).to.equal(addr1Balance);
+  describe("Deployment", function () {
+    it("Addr1 should have full supply", async function () {
+      let addr1Balance = await MyToken.balanceOf(addr1.address);
+      expect(await MyToken.totalSupply()).to.equal(addr1Balance);
+    });
+
+    it("S & Total should equal 0", async function () {
+      expect(await Staking.totalAssets()).to.equal(0);
+      expect(await Staking.s_num()).to.equal(0);
+      expect(await Staking.s_den()).to.equal(1);
+    });
   });
 
-  it("S & Total should equal 0", async function () {
-    expect(await Staking.Total()).to.equal(0);
-    expect(await Staking.S()).to.equal(0);
+  describe("Single user staking", function () {
+    it("Should stake 50 tokens", async function () {
+      await MyToken.approve(Staking.address, 50);
+      await Staking.stake(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.totalAssets()).to.equal(50);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(50);
+    });
+
+    it("Should distribute 50 tokens as reward", async function () {
+      await MyToken.approve(Staking.address, 50);
+      await Staking.stake(50, addr1.address);
+
+      await MyToken.approve(Staking.address, 50);
+      await Staking.distribute(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.totalAssets()).to.equal(50);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(100);
+
+      expect(await Staking.s_0_num(addr1.address)).to.equal(0);
+      expect(await Staking.s_0_den(addr1.address)).to.equal(1);
+      expect(await Staking.s_num()).to.equal(1);
+      expect(await Staking.s_den()).to.equal(1);
+    });
+
+    it("Should withdraw 50 token stake + 50 token reward", async function () {
+      await MyToken.approve(Staking.address, 50);
+      await Staking.stake(50, addr1.address);
+
+      await MyToken.approve(Staking.address, 50);
+      await Staking.distribute(50, addr1.address);
+
+      await Staking.unstake(100, addr1.address, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(0);
+      expect(await Staking.totalAssets()).to.equal(0);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(0);
+
+      let addr1Balance = await MyToken.balanceOf(addr1.address);
+      expect(await MyToken.totalSupply()).to.equal(addr1Balance);
+    });
+
+    it("Should recalculate stake when staking more", async function () {
+      await MyToken.approve(Staking.address, 50);
+      await Staking.stake(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.totalAssets()).to.equal(50);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(50);
+
+      await MyToken.approve(Staking.address, 50);
+      await Staking.distribute(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.totalAssets()).to.equal(50);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(100);
+
+      await MyToken.approve(Staking.address, 50);
+      await Staking.stake(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(150);
+      expect(await Staking.totalAssets()).to.equal(150);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(150);
+    });
   });
 
-  it("Should stake tokens", async function () {
-    await MyToken.approve(Staking.address, 50);
-    await Staking.Stake(addr1.address, 50);
+  describe("Multiple user staking", function () {
+    it("Should handle remainder of 3 token distribution", async function () {
+      // Gives addr1 100 tokens & addr2 50 token
+      await MyToken.transfer(addr1.address, 50);
+      await MyToken.transfer(addr2.address, 50);
 
-    expect(await Staking.Staked(addr1.address)).to.equal(50);
-    expect(await Staking.Total()).to.equal(50);
-    expect(await Staking.MaxUnstake(addr1.address)).to.equal(50);
-  });
+      // Stakes 50 tokens from addr1
+      await MyToken.approve(Staking.address, 50);
+      await Staking.stake(50, addr1.address);
 
-  it("Should distribute rewards", async function () {
-    await MyToken.approve(Staking.address, 50);
-    await Staking.Stake(addr1.address, 50);
+      // Stakes 50 tokens from addr2
+      await MyToken.connect(addr2).approve(Staking.address, 50);
+      await Staking.connect(addr2).stake(50, addr2.address);
 
-    await MyToken.approve(Staking.address, 50);
-    await Staking.Distribute(addr1.address, 50);
+      // Distributes 3 tokens to stakers
+      await MyToken.approve(Staking.address, 3);
+      await Staking.distribute(3, addr1.address);
 
-    expect(await Staking.Staked(addr1.address)).to.equal(50);
-    expect(await Staking.Total()).to.equal(50);
-    expect(await Staking.MaxUnstake(addr1.address)).to.equal(100);
-  });
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.staked(addr2.address)).to.equal(50);
+      expect(await Staking.totalAssets()).to.equal(100);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(51);
+      expect(await Staking.maxUnstake(addr2.address)).to.equal(51);
 
-  it("Should withdraw original stake + rewards", async function () {
-    await MyToken.approve(Staking.address, 50);
-    await Staking.Stake(addr1.address, 50);
+      // Withdraws all tokens from contract
+      await Staking.connect(addr1).unstake(51, addr1.address, addr1.address);
+      await Staking.connect(addr2).unstake(51, addr1.address, addr2.address);
 
-    await MyToken.approve(Staking.address, 50);
-    await Staking.Distribute(addr1.address, 50);
+      expect(await MyToken.balanceOf(Staking.address)).to.equal(1);
+    });
 
-    await Staking.Unstake(addr1.address, addr1.address, 100);
+    it("Should handle multiple stakes, unstakes, and distributions", async function () {
+      // Gives addr1 100 tokens & addr2 50 token
+      await MyToken.transfer(addr1.address, 100);
+      await MyToken.transfer(addr2.address, 50);
 
-    expect(await Staking.Staked(addr1.address)).to.equal(0);
-    expect(await Staking.Total()).to.equal(0);
-    expect(await Staking.MaxUnstake(addr1.address)).to.equal(0);
+      // Stakes 50 tokens from addr1
+      await MyToken.approve(Staking.address, 50);
+      await Staking.stake(50, addr1.address);
 
-    const addr1Balance = await MyToken.balanceOf(addr1.address);
-    expect(await MyToken.totalSupply()).to.equal(addr1Balance);
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.staked(addr2.address)).to.equal(0);
+      expect(await Staking.totalAssets()).to.equal(50);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(50);
+      expect(await Staking.maxUnstake(addr2.address)).to.equal(0);
+
+      // Distributes 50 tokens to stakers
+      await MyToken.approve(Staking.address, 50);
+      await Staking.distribute(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.staked(addr2.address)).to.equal(0);
+      expect(await Staking.totalAssets()).to.equal(50);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(100);
+      expect(await Staking.maxUnstake(addr2.address)).to.equal(0);
+
+      // Stakes 50 tokens from addr2
+      await MyToken.connect(addr2).approve(Staking.address, 50);
+      await Staking.connect(addr2).stake(50, addr2.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.staked(addr2.address)).to.equal(50);
+      expect(await Staking.totalAssets()).to.equal(100);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(100);
+      expect(await Staking.maxUnstake(addr2.address)).to.equal(50);
+
+      // Distributes 50 tokens to stakers
+      await MyToken.approve(Staking.address, 50);
+      await Staking.distribute(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(50);
+      expect(await Staking.staked(addr2.address)).to.equal(50);
+      expect(await Staking.totalAssets()).to.equal(100);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(125);
+      expect(await Staking.maxUnstake(addr2.address)).to.equal(75);
+
+      // Stakes 50 tokens from addr1
+      await MyToken.approve(Staking.address, 50);
+      await Staking.stake(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(175);
+      expect(await Staking.staked(addr2.address)).to.equal(50);
+      expect(await Staking.totalAssets()).to.equal(225);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(175);
+      expect(await Staking.maxUnstake(addr2.address)).to.equal(75);
+
+      // Distributes 50 tokens to stakers
+      await MyToken.approve(Staking.address, 50);
+      await Staking.distribute(50, addr1.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(175);
+      expect(await Staking.staked(addr2.address)).to.equal(50);
+      expect(await Staking.s_num()).to.equal(31);
+      expect(await Staking.s_den()).to.equal(18);
+      expect(await Staking.totalAssets()).to.equal(225);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(214);
+      expect(await Staking.maxUnstake(addr2.address)).to.equal(86);
+
+      // Withdraws all tokens from contract
+      await Staking.connect(addr1).unstake(214, addr1.address, addr1.address);
+      await Staking.connect(addr2).unstake(86, addr1.address, addr2.address);
+
+      expect(await Staking.staked(addr1.address)).to.equal(0);
+      expect(await Staking.staked(addr2.address)).to.equal(0);
+      expect(await Staking.totalAssets()).to.equal(0);
+      expect(await Staking.maxUnstake(addr1.address)).to.equal(0);
+      expect(await Staking.maxUnstake(addr2.address)).to.equal(0);
+
+      let addr1Balance = await MyToken.balanceOf(addr1.address);
+      expect(await MyToken.totalSupply()).to.equal(addr1Balance);
+    });
   });
 });
